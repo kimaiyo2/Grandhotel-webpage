4338cda0ac11e880fd6e09a729bb0c058634fa51
isaac kimaiyo <isaackimaiyo704@gmail.com>
2025-05-06 13:12:54 +0300

    first commit

---
+++ about-page.js
@@ -0,0 +1,11 @@
+let a=true;
+let b=18;
+if(a>=b){
+    console.log("eligible to vote.");
+}
+else if(a<=b){
+    console.log("not eligible to vote.");
+}
+else {
+    console.log("not a citizen");
+}
